828bf60d6c35c435d88becdf7428914bc4288bb7
leebow <leebow@protonmail.com>
2020-05-04 04:04:33 +0200

    Additional nedb-data-service unit tests added

--- test/unit/nedb-data-service-tests.js
+++ test/unit/nedb-data-service-tests.js
@@ -42,13 +42,112 @@ describe('nedb-data-service-tests', function () {
             data: { testKey: 'testValue' }
         };
 
+        let result = await dataService.upsert(testMsg);
+        expect(result.data).to.equal(testMsg.data);
+
+    });
+
+    it('successfully upserts data as a sibling', async () => {
+
+        const mockNedb = this.__mocker.mock(Nedb.prototype)
+            .withAsyncStub('findOne', {})
+            .withAsyncStub('update', {})
+            .create();
+
+        const utils = new Utils();
+
+        // system under test (using utils.traceMethodCalls to get detailed tracing)
+        const dataService = this.__utils.traceMethodCalls(DataService.create(this.__config, this.__logger, mockNedb, utils));
+
+        let testMsg = {
+            path: '/test',
+            data: { testKey: 'testValue' },
+            options: {
+                set_type: 'sibling'
+            }
+        };
+
+        let result = await dataService.upsert(testMsg);
+        expect(result.data).to.equal(testMsg.data);
+        expect(result._meta.path.split('/').length).to.equal(3);    // path should look something like: '/test/R39Nn_2hSnmrAtSta_VI9g-0'
+
+    });
+
+    it('successfully upserts a tag', async () => {
+
+        const mockNedb = this.__mocker.mock(Nedb.prototype)
+            .withAsyncStub('findOne', {})
+            .withAsyncStub('update', {})
+            .create();
+
+        const utils = new Utils();
+
+        // system under test (using utils.traceMethodCalls to get detailed tracing)
+        const dataService = this.__utils.traceMethodCalls(DataService.create(this.__config, this.__logger, mockNedb, utils));
+
+        let testMsg = {
+            path: '/test',
+            options: {
+                tag: 'test_tag'
+            }
+        };
+
+        let result = await dataService.upsert(testMsg);
+        expect(result.data).to.eql({});
+        expect(result._meta.path.split('/').length).to.equal(2);    // path should look something like: '/test/R39Nn_2hSnmrAtSta_VI9g-0'
+
+    });
+
+    it('throws an error if no data or path', async () => {
+
+        const mockNedb = this.__mocker.mock(Nedb.prototype)
+            .withAsyncStub('findOne', {})
+            .withAsyncStub('update', {})
+            .create();
+
+        const utils = new Utils();
+
+        // system under test (using utils.traceMethodCalls to get detailed tracing)
+        const dataService = this.__utils.traceMethodCalls(DataService.create(this.__config, this.__logger, mockNedb, utils));
+
+        let testMsg = {};
+
         try {
-            let result = await dataService.upsert(testMsg);
-            expect(result.data).to.equal(testMsg.data);
+            await dataService.upsert(testMsg);
         } catch (err) {
-            throw err;
+            expect(err.message).to.equal('No path defined; No data defined');
         }
 
     });
+
+    it('throws an error if data and tag present in same payload', async () => {
+
+        const mockNedb = this.__mocker.mock(Nedb.prototype)
+            .withAsyncStub('findOne', {})
+            .withAsyncStub('update', {})
+            .create();
+
+        const utils = new Utils();
+
+        // system under test (using utils.traceMethodCalls to get detailed tracing)
+        const dataService = this.__utils.traceMethodCalls(DataService.create(this.__config, this.__logger, mockNedb, utils));
+
+        let testMsg = {
+            data: { testKey: 'testValue' },
+            path: '/test',
+            options: {
+                tag: 'test_tag'
+            }
+        };
+
+        try {
+            await dataService.upsert(testMsg);
+        } catch (err) {
+            expect(err.message).to.equal('Cannot set tag with new data.');
+        }
+
+    });
+
+    // TODO: merge
 
 })
